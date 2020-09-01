29b957221c4035a2381eb537dee39dbc1ac8f58d
tristangraaff <67099158+tristangraaff@users.noreply.github.com>
2020-09-01 10:33:06 +0200

    Add files via upload

---
+++ front_end_opleiding/level_012/js_draaien_op_pc/script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy studenten")
+// Dit is een grote some
+/* Dit zijn twee
+interessante sommen */
+let multiplication = 1230941 * 1823794
+console.log("multiplication:", multiplication)
+let division = 637263 / 54
+console.log("division:", division)
